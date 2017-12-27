25f34eb47783851477697c523fd673265de4515d
Chris Villanueva <cvillanueva6@ucmerced.edu>
2017-12-26 20:43:41 -0800

    Safe areas accommodated. Bottom controls added using stack views

---
+++ MyFinances/Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Extensions.swift
+//  MyFinances
+//
+//  Created by Christopher Villanueva on 12/26/17.
+//  Copyright © 2017 Christopher Villanueva. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static var darkGreen = UIColor.init(red: 0, green: 100/255, blue: 0, alpha: 1)
+}
--- MyFinances/ViewController.swift
+++ MyFinances/ViewController.swift
@@ -31,10 +31,42 @@ class ViewController: UIViewController {
         return textView
     }()
     
+    /*
+     This container helps handle when shifting between landscape and portrait
+     by having our items as subviews of this top container and constrained
+     properly despite different dimensions
+    */
     let topImageContainerView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
+    }()
+    
+    private let previousButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("PREV", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.gray, for: .normal)
+        return button
+    }()
+    
+    private let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("NEXT", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.darkGreen, for: .normal)
+        return button
+    }()
+    
+    private let pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.currentPage = 0
+        pc.numberOfPages = 4
+        pc.currentPageIndicatorTintColor = .darkGreen
+        pc.pageIndicatorTintColor = .gray
+        return pc
     }()
 
     /*
@@ -44,13 +76,32 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         setupLayout()
+        setupBottonControls()
+    }
+    
+    fileprivate func setupBottonControls(){
+        
+        view.addSubview(previousButton)
+        
+        let bottomControlsStackView = UIStackView(arrangedSubviews: [previousButton, pageControl, nextButton])
+        bottomControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomControlsStackView.distribution = .fillEqually
+        view.addSubview(bottomControlsStackView)
+        
+        NSLayoutConstraint.activate([
+            bottomControlsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomControlsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bottomControlsStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bottomControlsStackView.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
     }
     
     /*
      private func setupLayout:
-        Add the subviews for our UI and
-        Use auto layout to add constraints in order
-        to format the UI as desired
+        Add the subviews for our UI and use auto layout to add constraints in order
+        to format the UI as desired. Our money image will be a subview of our top container view
+        so that it is properly constrained in both landscape and portrait
     */
     private func setupLayout(){
         
